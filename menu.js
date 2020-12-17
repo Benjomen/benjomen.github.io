c02c28a61d1862a95146b53fa4498d26aea8bdef
Benjomen <benjamin@wardenbach.net>
2020-12-17 15:28:53 +0100

    animemode v1

--- menu.js
+++ menu.js
@@ -9,10 +9,40 @@ let animemodeactive =false
 
 document.getElementById("animemodebutton").addEventListener("click", makeanime);
 
-function makeanime(){
+if(getCookie("animemode")=="true"){
+    makeanime()
+}
+ function makeanime(){
     if (animemodeactive==false){
         Array.from(document.getElementsByClassName("modetoggleable")).forEach(function(element) {element.classList.add("animemode")})
-    }else{ Array.from(document.getElementsByClassName("modetoggleable")).forEach(function(element) {element.classList.remove("animemode")})}
+        Array.from(document.getElementsByClassName("animemodepictures")).forEach(function(element) {element.style.visibility="visible"})
+    }else{
+         Array.from(document.getElementsByClassName("modetoggleable")).forEach(function(element) {element.classList.remove("animemode")})
+         Array.from(document.getElementsByClassName("animemodepictures")).forEach(function(element) {element.style.visibility="hidden"})
+    }
     animemodeactive= !animemodeactive
+    setCookie("animemode", animemodeactive, 365)
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
